062fac5ca07493dec615a9c8dac4b12f47c60de1
Tu <qingshi.tu@ubc.ca>
2023-01-27 17:50:43 -0800

    reorganize the files for the lca_calculator_bw2 tool

---
+++ lca_calculator_bw2/SE_config.py
@@ -0,0 +1,11 @@
+"""
+This config file contains the following information:
+	- path to the excel file containing "activities" and "lcia methods"
+	- path to the ecoinvent db
+"""
+
+LCA_MODELS = r"C:\Users\qtu2020\Documents\SE-EmbodiedImpacts\bw2_calc_input.xlsx"
+EI_DB_NAME = "ei35_cutoff" #"some random db"
+EI_DB_PATH = r"C:\Users\qtu2020\Desktop\ecoinvent 3.5_cutoff_ecoSpold02\datasets"
+EI_OVERVIEW_FILE_PATH = r"C:\Users\qtu2020\Documents\SE-EmbodiedImpacts\activity_overview_3.6_undefined_public_1.xlsx"
+OUTPUT_PATH = r"C:\Users\qtu2020\Documents\SE-EmbodiedImpacts\output"
